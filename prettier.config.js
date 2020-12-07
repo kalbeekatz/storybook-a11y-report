77171bd91ae53a3ea7d2d143e60fe518c9569c13
軽部勝仁 <karube5217@gmail.com>
2020-12-07 18:06:28 +0900

    linter & formatter config

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  overrides: [
+    {
+      files: '*.{md,yml}',
+      options: {
+        printWidth: 80,
+        semi: true,
+        singleQuote: false,
+        trailingComma: 'none',
+      },
+    },
+  ],
+}
