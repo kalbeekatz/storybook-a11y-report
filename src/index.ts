d93c60ebce26db83413beea54bcab7c4e2644331
katz <48142210+kalbeekatz@users.noreply.github.com>
2020-12-09 17:25:10 +0900

    feat log

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ import Axe, { ElementContext, Spec, RunOptions, getRules } from 'axe-core'
 import minimist from 'minimist'
 import minimatch from 'minimatch'
 import colors from 'colors'
+import ora from 'ora'
 import { errorText } from './constants'
 
 type Result = {
@@ -38,6 +39,7 @@ const argv = minimist(process.argv.slice(2), {
     e: 'exclude',
     f: 'filter',
     o: 'omit',
+    q: 'exit',
   },
 })
 const {
@@ -47,24 +49,32 @@ const {
   omit = [],
   storybookUrl = 'http://localhost:6006',
   outDir = '__report__',
+  exit,
 } = argv
 const a11yRules = getRules()
 const filters = flatten([filter]).reduce((acc: string[], givenId) => {
   if (a11yRules.some((rule) => rule.ruleId === givenId)) return acc.concat(givenId)
-  console.log(colors.yellow(`Confirm --filter option. A11y ID "${givenId}" is invalid.`))
+  console.log(
+    colors.yellow(`${colors.red('✗')} Confirm --filter option. A11y ID "${givenId}" is invalid.`),
+  )
   return acc
 }, [])
 const omits = flatten([omit]).reduce((acc: string[], givenId) => {
   if (a11yRules.some((rule) => rule.ruleId === givenId)) return acc.concat(givenId)
-  console.log(colors.yellow(`Confirm --omit option. A11y ID "${givenId}" is invalid.`))
+  console.log(
+    colors.yellow(`${colors.red('✗')} Confirm --omit option. A11y ID "${givenId}" is invalid.`),
+  )
   return acc
 }, [])
 const createReportMessage = (violationId: string, violations: Result[]) => {
   return violations
     .sort((a, b) => (a.storyId > b.storyId ? 1 : a.storyId < b.storyId ? -1 : 0))
     .reduce(
-      (acc, { storyId }) => (acc += `    ${storybookUrl}/?path=/story/${storyId}\n`),
-      `A11y ID: ${violationId}\ndescription: ${violations[0].description}\nDetected on:\n`,
+      (acc, { storyId }) =>
+        (acc += `    ${colors.blue.underline(`${storybookUrl}/?path=/story/${storyId}`)}\n`),
+      `${colors.bold(`A11y ID: ${violationId}`)}\ndescription: ${
+        violations[0].description
+      }\nDetected on:\n`,
     )
 }
 const createReport = (
@@ -104,13 +114,17 @@ const formatResults = (results: Result[][]) => {
     .reduce((acc, cur) => ({ ...acc, [cur]: grouped[cur] }), {})
   return sorted
 }
+const spinner1 = ora('now loading storybook...\n')
+const spinner2 = ora('now reporting...\n')
 ;(async function () {
   try {
+    spinner1.start()
     const connection = await new StorybookConnection({ storybookUrl }).connect()
     const storiesBrowser = await new StoriesBrowser(connection).boot()
     const allStories = await storiesBrowser.getStories()
     const stories = filterStories(allStories, flatten([include]), flatten([exclude]))
-    console.log(`Found ${colors.green(stories.length.toString())} stories.\n`)
+    spinner1.succeed(`Found ${colors.green(stories.length.toString())} stories.\n`)
+    spinner2.start()
     const workers = await Promise.all(
       [...Array(cpuLength - 1).keys()].map((i) => new StoryPreviewBrowser(connection, i).boot()),
     )
@@ -150,6 +164,7 @@ const formatResults = (results: Result[][]) => {
       })
       const results = await service.execute()
       const report = createReport(formatResults(results), filters, omits)
+      spinner2.stop()
       if (report) {
         console.log(report)
         await mkdirp(outDir)
@@ -158,13 +173,12 @@ const formatResults = (results: Result[][]) => {
           `filter: ${filters}\nomit: ${omits}\ninclude: ${include}\nexclude: ${exclude}\n\n` +
             report,
         )
-        console.error(
-          `You can check the report out here:\n    ${path.resolve(
-            __dirname,
-            `../${outDir}/a11y_report.md`,
+        console.log(
+          `You can check the report out here:\n    ${colors.blue.underline(
+            `${path.resolve(__dirname, `../${outDir}/a11y_report.md`)}`,
           )}`,
         )
-        process.exit(1)
+        if (exit) process.exit(1)
       } else {
         console.log(`\n✨ ✨ That's perfect, there is no a11y violation! ✨ ✨`)
       }
@@ -175,11 +189,14 @@ const formatResults = (results: Result[][]) => {
       )
       process.exit(1)
     } finally {
+      spinner1.stop()
+      spinner2.stop()
       await storiesBrowser.close()
       await Promise.all(workers.map((worker) => worker.close()))
       await connection.disconnect()
     }
   } catch (err) {
+    spinner1.stop()
     console.error(`${errorText} There is an error about connection:`, err.message)
     process.exit(1)
   }
